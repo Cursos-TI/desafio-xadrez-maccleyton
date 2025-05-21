b76a64f2970abe2a489b5a595a41b289b61af2da
Cleyton Duda Macedo <maccleyton@gmail.com>
2025-05-21 02:31:05 +0000

    etapa-2-nivel-aventureiro

--- desafio_xadrez.c
+++ desafio_xadrez.c
@@ -3,7 +3,7 @@
 // Coordenas do Tabuleiro: Colunas (a, b, c, d, e, f, g, h). Linhas (1, 2, 3, 4, 5, 6, 7, 8).
 
 int main() { 
-    int i;
+    int i, j;
     char coluna = 'a';
     int linha = 1;
 
@@ -16,7 +16,6 @@ int main() {
         printf("Direita -> %c%d\n", coluna, linha);
         i++;
     }
-
     printf("\n");
 
     // Bispo: Move-se 5 casas na diagonal (coluna e linha aumentam).
@@ -30,8 +29,10 @@ int main() {
         printf("Cima, Direita -> %c%d\n", coluna, linha);
         i++;
     } while (i <= 5);
-
     printf("\n");
+
+    /* O enunciado pede para movimentar a Rainha 8 casas à esquerda, porém como o tabuleiro tem apenas
+    8x8 casas, limitei o movimento em 7 casas para não ultrapassar os limites do tabuleiro */
 
     // Rainha: Move-se 7 casas à esquerda (coluna diminui).
     coluna = 'h';
@@ -41,8 +42,23 @@ int main() {
         coluna--; // Move a colua de 'h' para 'g', 'f', ...
         printf("Esquerda -> %c%d\n", coluna, linha);
     }
-
     printf("\n");
 
+    // Cavalo: Move-se 2 casas para baixo e 1 casa à esquerda (coluna diminui).
+    // for para loop externo - while para loop interno
+    coluna = 'f';
+    linha = 8;
+    printf("Cavalo: Posição inicial (f8)\n");
+    for (i = 1; i <= 2; i++) {
+        linha--; // move a linha de 8 para 7, 6...
+        printf("Baixo -> %c%d\n", coluna, linha);
+
+        j = i;
+        while (j == 2) {
+            coluna--;
+            printf("Esquerda -> %c%d\n", coluna, linha);
+            j++; // força a saída do loop após uma execução
+        }
+    }
     return 0;
 }
